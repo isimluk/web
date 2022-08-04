136aeadc178d9f6bb57b2e4e38a57451716b9f1f
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-08-04 15:50:57 +0200

    Archive instead of deleting

--- lib/trento/application/usecases/rollup/rollup.ex
+++ lib/trento/application/usecases/rollup/rollup.ex
@@ -8,7 +8,7 @@ defmodule Trento.Rollup do
 
     Postgrex.transaction(pid, fn conn ->
       with :ok <- Trento.EventStore.delete_snapshot(aggregate_id, conn: conn),
-           :ok <- Trento.EventStore.delete_stream(aggregate_id, :any_version, :hard, conn: conn) do
+           {:ok, _} <- archive_stream(conn, aggregate_id) do
         Trento.EventStore.append_to_stream(
           aggregate_id,
           :any_version,
@@ -25,5 +25,15 @@ defmodule Trento.Rollup do
         )
       end
     end)
+  end
+
+  defp archive_stream(conn, stream_id) do
+    now = DateTime.utc_now() |> DateTime.to_unix()
+
+    Postgrex.query(
+      conn,
+      "UPDATE streams SET stream_uuid = 'archived-#{stream_id}-#{now}' WHERE stream_uuid = '#{stream_id}';",
+      []
+    )
   end
 end
