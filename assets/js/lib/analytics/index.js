b947a8c07b6771da9970443e58fe28d726d09874
Alessio Biancalana <alessio.biancalana@suse.com>
2023-04-14 16:32:07 +0200

    Retrieve installation id and push it

--- assets/js/lib/analytics/index.js
+++ assets/js/lib/analytics/index.js
@@ -1,7 +1,13 @@
 import posthog from 'posthog-js';
 
+import config from '@lib/config';
+
+const installationID = config.get('installationID');
+
 posthog.init('phc_1kR0B0FKHGH5XRkfVOCYx7bJYyP47zN3wdyNrDCLn1f', {
   api_host: 'https://metrics.trento.suse.com',
 });
 
-export const capture = (event, payload) => posthog.capture(event, payload);
+export const capture = (event, payload) => {
+  posthog.capture(event, { ...payload, installationID });
+};
--- lib/trento_web/controllers/page_controller.ex
+++ lib/trento_web/controllers/page_controller.ex
@@ -1,13 +1,17 @@
 defmodule TrentoWeb.PageController do
   use TrentoWeb, :controller
 
+  alias Trento.Installation
+
   def index(conn, _params) do
     grafana_public_url = Application.fetch_env!(:trento, :grafana)[:public_url]
     check_service_base_url = Application.fetch_env!(:trento, :checks_service)[:base_url]
+    installation_id = Installation.get_installation_id()
 
     render(conn, "index.html",
       grafana_public_url: grafana_public_url,
-      check_service_base_url: check_service_base_url
+      check_service_base_url: check_service_base_url,
+      installation_id: installation_id
     )
   end
 end
