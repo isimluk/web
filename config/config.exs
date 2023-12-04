67d0c9cf97ea272df06739fb90f1e5eba3e58e64
Nelson Kopliku <nelson.kopliku@suse.com>
2023-12-04 11:05:15 +0100

    Poc SUMA integration

--- config/config.exs
+++ config/config.exs
@@ -174,6 +174,11 @@ config :trento,
   api_key_authentication_enabled: true,
   jwt_authentication_enabled: true
 
+config :trento, :suma,
+  user: "",
+  password: "",
+  base_url: ""
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
